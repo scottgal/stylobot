7b0a4e005fa36eb2c507c0e670299e7b6fe6d591
mostlylucid <scott.galloway@gmail.com>
2026-05-06 13:38:41 +0100

    fix(packs): atomic Clear() in PackRegistry via Interlocked.Exchange

--- src/Mostlylucid.BotDetection/Packs/PackRegistry.cs
+++ src/Mostlylucid.BotDetection/Packs/PackRegistry.cs
@@ -4,15 +4,11 @@ namespace Mostlylucid.BotDetection.Packs;
 
 public sealed class PackRegistry<T> : IEnumerable<T>
 {
-    private readonly ConcurrentBag<T> _items = new();
+    private ConcurrentBag<T> _items = new();
 
     public void Add(T item) => _items.Add(item);
 
-    public void Clear()
-    {
-        while (!_items.IsEmpty)
-            _items.TryTake(out _);
-    }
+    public void Clear() => Interlocked.Exchange(ref _items, new ConcurrentBag<T>());
 
     public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
